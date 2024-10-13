cae2154a025665222446a0ccaa70c04d37c0490f
Abel Mebratu Yehuala <109806173+Abel-Mebratu@users.noreply.github.com>
2024-10-13 02:41:08 +0200

    Add files via upload

---
+++ int.m
@@ -0,0 +1,19 @@
+clearvars;
+clc;
+global  R wr Tc am bm gammax gammar gammaphi khat0 lhat0 thetahat0 bbar
+Tc = 0.1;
+sigma2e = 0.0;
+am =-4.0;
+bm = 4.0;
+R = 0.15;
+wr = 0.25;
+bbar = -1;
+gammax = 0.0005;
+gammar = 0.0005;
+gammaphi = 0.1;
+p = -1/(2*am)
+khat0 = 0;
+lhat0 = 0; 
+thetahat0 = 0;
+ke = 10; 
+
